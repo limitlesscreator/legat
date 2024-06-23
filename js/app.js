fbf8375b2ce38755b095b55e4c5e5b7c43d58435
limitlesscreator <0428vlad0428@mail.ru>
2024-06-24 00:41:03 +0300

    before transport to header_footer block (any questions)

--- js/app.js
+++ js/app.js
@@ -63,12 +63,95 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   // файл загружен
-  document.getElementById('file-input').addEventListener('change', function() {
-    var fileLabel = document.getElementById('file-label-text');
-    if (this.files.length > 0) {
-      fileLabel.textContent = 'файл загружен';
-    } else {
-      fileLabel.textContent = 'Прикрепить файл';
+    if (document.getElementById('file-input')){
+      document.getElementById('file-input').addEventListener('change', function() {
+        var fileLabel = document.getElementById('file-label-text');
+        if (this.files.length > 0) {
+          fileLabel.textContent = 'файл загружен';
+        } else {
+          fileLabel.textContent = 'Прикрепить файл';
+        }
+      });
+    }
+});
+
+
+
+
+
+// Intersection Observer for titleSection
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+      observer.unobserve(entry.target);
     }
   });
+}, observerOptions);
+
+document.querySelectorAll('.titleSection').forEach(section => {
+  observer.observe(section);
 });
+
+
+//animation :show images one by one imgS1Elem
+document.addEventListener("DOMContentLoaded", function() {
+  const imgElems = document.querySelectorAll('.imgS1Elem');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        setTimeout(() => {
+          entry.target.classList.add('in-view');
+        }, index * 300); // Delay each image by 0.3s
+        observer.unobserve(entry.target); // Stop observing once it has become visible
+      }
+    });
+  }, {
+    threshold: 0.1 // Trigger when 10% of the element is visible
+  });
+
+  imgElems.forEach(img => {
+    observer.observe(img);
+  });
+});
+
+
+//contact yandex map
+ymaps.ready(init);
+
+function init() {
+  // Map 1
+  var myMap1 = new ymaps.Map("map1", {
+    center: [55.731663, 37.655407], // Coordinates for the first address
+    zoom: 16,
+    controls: ['zoomControl', 'typeSelector', 'fullscreenControl', 'routeButtonControl']
+  });
+
+  var placemark1 = new ymaps.Placemark([55.731663, 37.655407], {
+    hintContent: 'г. Москва, Скотопрогонная, 35 стр. 5',
+    balloonContent: '<strong>г. Москва, Скотопрогонная, 35 стр. 5</strong><br>Пн-Вс 8:00-18:00'
+  });
+
+  myMap1.geoObjects.add(placemark1);
+
+  // Map 2
+  var myMap2 = new ymaps.Map("map2", {
+    center: [55.794007, 37.702875], // Coordinates for the second address
+    zoom: 16,
+    controls: ['zoomControl', 'typeSelector', 'fullscreenControl', 'routeButtonControl']
+  });
+
+  var placemark2 = new ymaps.Placemark([55.794007, 37.702875], {
+    hintContent: 'Москва, ул. Ткацкая, 5 стр. 4',
+    balloonContent: '<strong>Москва, ул. Ткацкая, 5 стр. 4</strong><br>Пн-Вс 9:00-21:00'
+  });
+
+  myMap2.geoObjects.add(placemark2);
+}
